4d360713d661c6c9cb5e8582750dde5c024bceff
sakib07q <sakib.swroar2018@northsouth.edu>
2019-04-09 12:09:28 +0600

    templates added

--- Healthcare/Healthcare/urls.py
+++ Healthcare/Healthcare/urls.py
@@ -1,7 +1,7 @@
-"""Healthcare URL Configuration
+"""healthcare URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
+import healthservice
+from healthservice.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$',healthservice.views.index, name='index_page'),
 ]
